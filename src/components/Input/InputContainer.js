147b86371150bad65344bfa8ed501ebc87d982d5
HarunSarii <harunsari14@gmail.com>
2021-09-09 12:41:31 +0300

    Add InputContainer

---
+++ src/components/Input/InputContainer.js
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import { Collapse, Paper, Typography } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import InputCard from './InputCard';
+
+
+const useStyle = makeStyles((theme) => ({
+    root: {
+        marginTop: theme.spacing(2)
+    },
+    addCard: {
+        padding: theme.spacing(1, 1, 1, 2),
+        margin: theme.spacing(0, 1, 1, 1),
+        backgroundColor: '#e6dfdf',
+        '&:hover': {
+            backgroundColor: '#ACA7A7',
+        }
+    }
+}))
+
+function InputContainer() {
+    const classes = useStyle();
+    const [open, setOpen] = useState(false);
+
+    return (
+        <div className={classes.root}>
+            <Collapse in={open}>
+                <InputCard setOpen={setOpen} />
+            </Collapse>
+            <Collapse in={!open}>
+                <Paper className={classes.addCard} elevation={0} onClick={() => setOpen(!open)} >
+                    <Typography>+ Add a Card</Typography>
+                </Paper>
+            </Collapse>
+        </div>
+    )
+}
+
+export default InputContainer
