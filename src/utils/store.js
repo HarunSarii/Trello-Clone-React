eed7ad291d7b87f94456336740a98c1661a2abd8
HarunSarii <harunsari14@gmail.com>
2021-09-09 13:24:27 +0300

    Add data (store)

---
+++ src/utils/store.js
@@ -0,0 +1,26 @@
+const cards = [
+    {
+        id: 'card-1',
+        content: 'Make an early reservation',
+    },
+    {
+        id: 'card-2',
+        content: 'Prepare car for road trip',
+    },
+    {
+        id: 'card-3',
+        content: 'Search for beautiful places to see',
+    },
+];
+
+const data = {
+    lists: {
+        'list1': {
+            id: 'list-1',
+            title: 'Todo',
+            cards,
+        }
+    },
+    listIds: ['list-1'],
+
+}
